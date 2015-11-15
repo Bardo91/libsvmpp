503ab869fdf25cc61563881932a74031e0bccf3e
Bardo91 <19Bardo91@gmail.com>
2015-11-15 01:54:36 +0100

    Modified LICENSE file. Coded TrainSet struct and Query struct

--- src/libsvmpp/Svmpp.cpp
+++ src/libsvmpp/Svmpp.cpp
@@ -1,5 +1,107 @@
-//
-//
-//
-//
-//
+//////////////////////////////////////////////////////////////////////////////////////////
+//																						//
+//	The MIT License (MIT)																//
+//																						//
+//	Copyright (c) 2015 Pablo Ramon Soria												//
+//																						//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy		//
+//	of this software and associated documentation files (the "Software"), to deal		//
+//	in the Software without restriction, including without limitation the rights		//
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell			//
+//	copies of the Software, and to permit persons to whom the Software is				//
+//	furnished to do so, subject to the following conditions:							//
+//																						//
+//	The above copyright notice and this permission notice shall be included in all		//
+//	copies or substantial portions of the Software.										//
+//																						//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR			//
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,			//
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE			//
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER				//
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,		//
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE		//
+//	SOFTWARE.																			//
+//																						//
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include "Svmpp.h"
+
+#include <cassert>
+
+namespace svmpp {
+	//-----------------------------------------------------------------------------------------------------------------
+	// Train struct
+	//-----------------------------------------------------------------------------------------------------------------
+	void TrainSet::addEntry(double * _x, double _y) {
+		// not implemented yet.
+	}
+
+	//-----------------------------------------------------------------------------------------------------------------
+	void TrainSet::addEntry(const std::vector<double>& _x, double _y) {
+		mX.push_back(_x);
+		mY.push_back(_y);
+	}
+
+	//-----------------------------------------------------------------------------------------------------------------
+	void TrainSet::addEntries(const std::vector<std::vector<double>>& _X, const std::vector<double>& _Y){
+		mX.insert(mX.end(), _X.begin(), _X.end());
+		mY.insert(mY.end(), _Y.begin(), _Y.end());
+	}
+
+	//-----------------------------------------------------------------------------------------------------------------
+	TrainSet::Problem TrainSet::problem() const {
+		Problem problem;
+		
+		assert(mX.size() == mY.size());
+
+		// Set size of train set.
+		problem.l = mX.size();
+
+		// Set labels of data
+		problem.y = new double[problem.l];
+		for (unsigned i = 0; i < mY.size(); i++) {
+			problem.y[i] = mY[i];
+		}
+
+		// Set data
+		int dims = mX[0].size();
+		problem.x = new svm_node*[problem.l];
+		for (unsigned i = 0; i < mX.size(); i++) {
+			problem.x[i] = new svm_node[dims+1];
+			for (unsigned j = 0; j < dims; j++) {
+				problem.x[i][j].index = j;
+				problem.x[i][j].value = mX[i][j];
+			}
+			problem.x[i][dims].index = -1;
+		}
+
+		return problem;
+	}
+
+
+	//-----------------------------------------------------------------------------------------------------------------
+	// Query struct
+	//-----------------------------------------------------------------------------------------------------------------
+	Query::Query(double * _x) {
+		// 666 Not implemented yet.
+	}
+
+	//-----------------------------------------------------------------------------------------------------------------
+	Query::Query(const std::vector<double>& _x) {
+		int dims = _x.size();
+		mNode = new svm_node[dims+1];
+		for (unsigned i = 0; i < dims; i++) {
+			mNode[i].index = i;
+			mNode[i].value = _x[i];
+		}
+		mNode[dims].index = -1;
+	}
+
+	//-----------------------------------------------------------------------------------------------------------------
+	Query::Node Query::node() const {
+		return mNode;
+	}
+
+}	// namespace svmpp
+
--- src/libsvmpp/Svmpp.h
+++ src/libsvmpp/Svmpp.h
@@ -1,8 +1,28 @@
-//
-//
-//
-//
-//
+//////////////////////////////////////////////////////////////////////////////////////////
+//																						//
+//	The MIT License (MIT)																//
+//																						//
+//	Copyright (c) 2015 Pablo Ramon Soria												//
+//																						//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy		//
+//	of this software and associated documentation files (the "Software"), to deal		//
+//	in the Software without restriction, including without limitation the rights		//
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell			//
+//	copies of the Software, and to permit persons to whom the Software is				//
+//	furnished to do so, subject to the following conditions:							//
+//																						//
+//	The above copyright notice and this permission notice shall be included in all		//
+//	copies or substantial portions of the Software.										//
+//																						//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR			//
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,			//
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE			//
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER				//
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,		//
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE		//
+//	SOFTWARE.																			//
+//																						//
+//////////////////////////////////////////////////////////////////////////////////////////
 
 
 #ifndef LIBSVMPP_SVMPP_H_
@@ -20,24 +40,25 @@ namespace svmpp {
 	public:
 		typedef svm_problem Problem;
 		
-		bool addEntry(double *_x, double _y);
-		bool addEntry(const std::vector<double> &_x, double _y);
+		void addEntry(double *_x, double _y);
+		void addEntry(const std::vector<double> &_x, double _y);
 
-		bool addEntries(const std::vector<std::vector<double>> &_X, const std::vector<double> &_Y);
+		void addEntries(const std::vector<std::vector<double>> &_X, const std::vector<double> &_Y);
 
 		Problem problem() const;
 	private:
-		Problem mProblem;
+		std::vector<std::vector<double>>	mX;
+		std::vector<double>					mY;
 	};	// Struct TrainSet
 
 
 	//-----------------------------------------------------------------------------------------------------------------
 	struct Query {
 	public:
-		typedef svm_node Node;
+		typedef svm_node *Node;
 
-		Query(double *_x, double _y);
-		Query(const std::vector<double> &_x, double _y);
+		Query(double *_x);
+		Query(const std::vector<double> &_x);
 
 		Node node() const;
 	private:
