b7d220cfa2da7a35f49f5288d9f5b27d40776310
Bardo91 <19Bardo91@gmail.com>
2015-11-15 02:10:57 +0100

    Cleaned rbf test.

--- tests/rbfKernel/src/rbfKernel.cpp
+++ tests/rbfKernel/src/rbfKernel.cpp
@@ -10,42 +10,5 @@ using namespace std;
 using namespace svmpp;
 
 int main(int _argc, char **_argv) {
-	vector<double> x1 = {0,0};
-	vector<double> x2 = {0,1};
-	vector<double> x3 = {1,0};
-	vector<double> x4 = {1,1};
-
-	TrainSet set;
-	set.addEntry(x1, 0);
-	set.addEntry(x2, 0);
-	set.addEntry(x3, 0);
-	set.addEntry(x4, 1);
-
-	Svm svm;
-
-	// Setting parameters
-	Svm::Params params;
-	params.svm_type = C_SVC;
-	params.kernel_type = LINEAR;
-	params.cache_size = 100;
-	params.C = 1;
-	params.eps = 1e-5;
-	params.p = 0.1;
-	params.shrinking = 0;
-	params.probability = 0;
-	params.nr_weight = 0;
-	params.weight_label = nullptr;
-	params.weight = nullptr;
-
-	// Train
-	svm.train(params, set);
 	
-	// Test
-	Query query1({2,2});
-	double res = svm.predict(query1);
-	assert(res == 1);
-
-	Query query2({0,0});
-	res = svm.predict(query2);
-	assert(res == 0);
 }
